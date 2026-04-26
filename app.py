9a487fac744bf3f315f40120ba04b0913cc46028
dharmangi shah <shahdharmangi@gmail.com>
2026-04-26 19:16:13 +0530

    fix static routing

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__, static_folder="static", static_url_path="")
 CORS(app)
 
 UPLOAD_FOLDER = tempfile.gettempdir()
